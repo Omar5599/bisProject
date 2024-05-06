1385d2cc164ed2f8eb55bdb0d921a12823db7443
Omar5599 <133118920+Omar5599@users.noreply.github.com>
2024-05-06 20:02:31 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const url = 'mongodb://127.0.0.1:27017/shoppingApplication';
+const url = 'mongodb+srv://ahmedaltahtawy:ahmed1310ahmed@cluster0.lq6aueb.mongodb.net/?retryWrites=true&w=majority';
 
 const connectDb = () => {
     mongoose.connect(url)
